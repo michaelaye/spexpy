34e229b5047eb826d2f0646b4da21a9905f6de45
K.-Michael Aye <kmichael.aye@gmail.com>
2014-09-30 13:43:22 -0700

    putting both functionalities for guidedog and bigdog SPEX movie maving into one file and renaming it properly

--- IRTF_movie_maker.py
+++ IRTF_movie_maker.py
@@ -14,30 +14,64 @@ def read_scan1(fname):
     return data[248:394, :]
 
 
+def read_guide(fname):
+    return pyfits.getdata(fname, 0)
+
+
+def determine_data_type(fitsfiles):
+    xsize = pyfits.getheader(fitsfiles[0])['NAXIS1']
+    if xsize == 512:
+        return 'guide'
+    elif xsize == 1024:
+        return 'spex'
+    else:
+        return 'unknown'
+
+
+class TypeSettings(object):
+    def __init__(self, typestring):
+        if typestring == 'guide':
+            self.figsize = (10, 10)
+            self.aspect = 'equal'
+            self.zeroshape = (512, 512)
+            self.cmap = 'gray'
+            self.vmax = 2000
+        elif typestring == 'spex':
+            self.figsize = (10, 8)
+            self.aspect = 'auto'
+            self.zeroshape = (146, 1024)
+            self.cmap = 'hot'
+            self.vmax = None
+
+
 def main(root, fname_base, outfname):
     searchterm = root + '/' + fname_base + '*.fits'
     print(searchterm)
     fitsfiles = glob.glob(searchterm)
     fitsfiles.sort()
+    datatype = determine_data_type(fitsfiles)
+    settings = TypeSettings(datatype)
+
     FFMpegWriter = manimation.writers['ffmpeg']
-    metadata = dict(title='IRTF Scan 1', artist='Matplotlib',
-                    comment='Scan 1, rows 248:394')
+    metadata = dict(title='IRTF ' + outfname, artist='Matplotlib',
+                    comment='IRTF' + str(settings.zeroshape))
     writer = FFMpegWriter(fps=5, metadata=metadata)
 
-    fig = plt.figure(figsize=(10, 8))
-    handle = plt.imshow(np.zeros((146, 1024)), cmap='hot',
-                        interpolation='nearest', aspect='auto')
+    fig = plt.figure(figsize=settings.figsize)
+    handle = plt.imshow(np.zeros(settings.zeroshape), cmap=settings.cmap,
+                        interpolation='nearest', aspect=settings.aspect,
+                        vmax=2000)
 
     tomovie = fitsfiles
     with writer.saving(fig, outfname+".mp4", 100):
         for fitsfile in tomovie:
             print(fitsfile)
             no = fitsfile.split('-')[1][:5]
-            handle.get_axes().set_title(no)
+            time = pyfits.getheader(fitsfile)['TIME_OBS']
+            handle.get_axes().set_title(no + ' ' + time)
             data = read_scan1(fitsfile)
             filtered = median_filter(data, 2)
             handle.set_data(filtered)
-            handle.autoscale()
             writer.grab_frame()
 
 
